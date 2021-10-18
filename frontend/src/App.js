90c4cc8c23831cdc64f7231bc0496fa85be2a036
Victor Kalinkin <vakalinkin@hotmail.com>
2021-10-18 14:39:22 -0700

    completed basic app functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import React from "react";
+import Typeahead from "./Typeahead"
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Store Search</h1>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      storeData: [] ,
+      isLoading: true
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost/backend/stores')
+      .then(response => response.json())
+      .then(data => this.setState({ storeData: data, isLoading: false}));
+  }
+
+  render(){
+    console.log('this.state 2:', this.state);
+    console.log('this.state.storeData', this.state.storeData);
+    console.log('this.state.storeData.stores', this.state.storeData.stores);
+    const st = this.state.storeData.stores;
+    console.log('st: ', st);
+
+    return this.state.isLoading
+      ? <p>Loading</p>
+      :
+      <div>
+        <h1>Store Search</h1>
+        <Typeahead
+          suggestions={st.map(x => x.name)}
+        />
+      </div>
+
+  }
+};
 
 export default App;
